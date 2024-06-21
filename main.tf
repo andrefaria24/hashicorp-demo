226f1d61358b719f5b615088f958e2b80e4f0f39
Andre Faria <andre.faria.24@gmail.com>
2024-06-21 08:26:30 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+
+provider "aws"{
+    region = var.aws_region
+    allowed_account_ids = var.aws_accounts
+}
+
 module "aws_vpc"{
     source = "terraform-aws-modules/vpc/aws" # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/
     
