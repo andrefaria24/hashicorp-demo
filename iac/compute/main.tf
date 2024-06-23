a07d02c2ad4fb0063e03fa3a2988a643c18bf128
Andre Faria <andre.faria.24@gmail.com>
2024-06-23 10:59:37 -0400

    added compute iac logic

---
+++ iac/compute/main.tf
@@ -0,0 +1,49 @@
+terraform {
+    cloud {
+        organization = "ACME_andre_faria"
+
+        workspaces {
+            name = "ws-compute"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.55.0"
+        }
+    }
+}
+
+data "terraform_remote_state" "network"{
+    backend = "remote"
+
+    config = {
+        organization = "ACME_andre_faria"
+        workspaces = {
+            name = "ws-networking"
+        }
+  }
+}
+
+provider "aws" {
+    region = var.aws_region
+    allowed_account_ids = var.aws_accounts
+}
+
+resource "aws_instance" "web_server" {
+  ami           = var.webserver_ami
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  subnet_id = var.aws_pub_subnets[0]
+  security_groups = [data.terraform_remote_state.network.outputs.aws_sg_http_id]
+}
+
+resource "aws_eip" "web_server_eip" {
+  depends_on = [aws_instance.web_server]
+}
+
+resource "aws_eip_association" "web_server_eip_assoc" {
+  allocation_id = aws_eip.web_server_eip.allocation_id
+  instance_id = aws_instance.web_server.id
+}
---
+++ iac/compute/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_accounts" {
+    description = "AWS accounts allowed for resource provisioning"
+    type = list(string)
+    default = []
+}
+
+variable "aws_region" {
+    description = "AWS region resources will reside in"
+    type = string
+    default = ""
+}
+
+variable "aws_pub_subnets" {
+    description = "Public subnets CIDR blocks"
+    type = list(string)
+    default = []
+}
+
+variable "key_name" {
+    type = string
+    default = ""
+}
+
+variable "webserver_ami" {
+    type = string
+    default = ""
+}
